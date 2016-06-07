60176685ac8964cd4b909bf702f84f86947244db
Jordan <jminjie@umich.edu>
2016-06-07 14:48:06 +0000

    Add restore and incognito

--- index.js
+++ index.js
@@ -6,36 +6,40 @@
 
 module.exports = (pluginContext) => {
     const logger = pluginContext.logger;
+    const exec = require('child_process').exec;
    
-    var count = 1; 
-    var currentRes = null;
-
     function search(query, res) {
-        res.add({
-            id: "add",
-            title: "Add the shortcut "+query+" <span style='width:100px;height:100px;'</span>",
-            desc: "<span style='width:100px;height:100px;'</span>",
-            payload: query
-        });
-        currentRes = res;
-    }
-    function execute(id, payload) {
-        if (id=="exec") {
-            logger.log("executing roll!");
-            try {
-                var result = d20roll(payload);
-                if (isNaN(result)) {
-                    throw "NaN";
-                }
-            } catch (err) {
-                var result = 'Invalid notation.';
-            }
-            currentRes.add({
-                id: "roll_" + count,
-                title: result+" <span style='width:100px;height:100px;'</span>",
+        if (query == 'n') {
+            logger.log("Starting incognito");
+            res.add({
+                id: "incognito",
+                title: "Start Chrome in incognito mode<span style='width:100px;height:100px;'</span>",
                 desc: "<span style='width:100px;height:100px;'</span>",
             });
-            count++;
+        } else if (query == 'r') {
+            logger.log("Starting restore");
+            res.add({
+                id: "restore",
+                title: "Restore last Chrome session <span style='width:100px;height:100px;'</span>",
+                desc: "<span style='width:100px;height:100px;'</span>",
+            });
+        } else {
+            logger.log("Starting default"); 
+            res.add({
+                id: "noflag",
+                title: "Start Chrome <span style='width:100px;height:100px;'</span>",
+                desc: "<span style='width:100px;height:100px;'</span>",
+                payload: query
+            });
+        }
+    }
+    function execute(id, payload) {
+        if (id=="incognito") {
+            exec("START chrome --incognito");
+        } else if (id == "restore") {
+            exec("START chrome --restore-last-session");
+        } else {
+            exec("START chrome" + payload);
         }
     }
 return { search, execute };
