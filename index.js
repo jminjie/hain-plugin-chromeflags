ffd521ff7d76d7249a839a38838f8e659208c72d
Jordan <jminjie@umich.edu>
2016-06-12 18:43:23 -0400

    Move definition

--- index.js
+++ index.js
@@ -4,24 +4,24 @@
  * @license MIT
  */
 
-// Starts Google Chrome; if error, try full default path
-// Example calls:
-//      runChrome("--incognito");
-//      runChrome("--restore-last-session");
-function runChrome(flags) { 
-    const FULL_PATH = 'C:\\"Program Files (x86)"\\Google\\Chrome\\Application\\chrome.exe ';
-    var returnCode = exec("START chrome "+flags, function (error) {
-        if (error) {
-            logger.log("Error caught on START. Running with default full path.");
-            exec('START '+FULL_PATH+flags);
-        }
-    });
-}
-
 module.exports = (pluginContext) => {
     const logger = pluginContext.logger;
     const exec = require('child_process').exec;
    
+    // Starts Google Chrome; if error, try full default path
+    // Example calls:
+    //      runChrome("--incognito");
+    //      runChrome("--restore-last-session");
+    function runChrome(flags) { 
+        const FULL_PATH = 'C:\\"Program Files (x86)"\\Google\\Chrome\\Application\\chrome.exe ';
+        var returnCode = exec("START chrome "+flags, function (error) {
+            if (error) {
+                logger.log("Error caught on START. Running with default full path.");
+                exec('START '+FULL_PATH+flags);
+            }
+        });
+    }
+
     function search(query, res) {
         query = query.trim();
         if (query == 'n' || query == '-n') {
